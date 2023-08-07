3f5c029ce2fd59d4da540b47bb339912af957309
lionelcopeland <104397929+lionelcopeland@users.noreply.github.com>
2023-08-07 16:21:59 -0500

    Update passgen.py
    Removed extra text

--- passgen.py
+++ passgen.py
@@ -11,11 +11,4 @@ special = string.punctuation
 
 generate_password(10)
 
-'''
-the mock up has SIMON# but it is not in the additional information 
-DBHRef# is in the additional information but I don't see is on the mock up
-The word Insuance is misspelled 
 
-patient 4074768 - I changed the Institution to have an L on the end and I saved it, reopend it and it was still there
-
-'''

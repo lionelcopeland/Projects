8067057162e002198d38c33ce4fab12d781553a2
lionelcopeland <104397929+lionelcopeland@users.noreply.github.com>
2023-10-04 16:58:52 -0500

    Add files via upload
    Two files for Python Messenger Chat App

---
+++ client.py
@@ -0,0 +1,18 @@
+
+
+import socket
+
+client = socket.socket(socket.AF INET, socket.SOCK_STREAM)
+
+client.connect(("localhost", 9999))
+
+done = False
+
+while not done:
+    client.send(input"Message: ").encode('utf-8'))
+    msg = client.recv(1024).decode('utf-8')
+    if msg == "quit":
+        done = True
+    
+    else:
+        print(msg)
